d3c3c86b98fb01bfbf8a750bd320a741baefbd8e
TheAmanM <75181008+TheAmanM@users.noreply.github.com>
2025-04-23 02:36:21 -0400

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Deploy instantly with Vercel, the sponsor of this website.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
