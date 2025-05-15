4c0bba40ac772fdcdbd38e5f245a4639a067cf79
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-05-14 23:54:19 -0400

    refactor: swap anon key to service key

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createClient() {
 
     return createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!, // changed from anon key to allow admin access to rows
         {
             cookies: {
                 getAll() {
