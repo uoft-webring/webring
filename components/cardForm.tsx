728dcf442978a339d03dc8755a56f881a4112cd5
amanmeherally <dacyerdash@gmail.com>
2025-05-28 10:51:18 -0400

    Finally did shadcn theming

--- components/cardForm.tsx
+++ components/cardForm.tsx
@@ -21,7 +21,7 @@ export default function CardForm({
     cardDescription?: string;
 } & React.ComponentPropsWithoutRef<"div">) {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <div className="flex flex-col gap-6">
                     <Card className="gap-4">
