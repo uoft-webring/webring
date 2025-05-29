baa66a46c5bab9664444502c6379fe55bb810b32
amanmeherally <dacyerdash@gmail.com>
2025-05-29 18:32:17 -0400

    Modified loading component to remove SVG error

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-// import loadingAnimation from "@/assets/loading-animation.json";
-import loader from "@/assets/loading-animation.svg"; //TODO: Fix
+import loadingAnimation from "@/assets/loading-animation.json";
+// import loader from "@/assets/loading-animation.svg"; //TODO: Fix
 import Image from "next/image";
 // Importing Lottie, importing directly makes Lottie
 // think SSR is being used which causes errors
-// import dynamic from "next/dynamic";
-// const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 // import Lottie from "lottie-react";
 
 //TODO: fix CSS styling, currently unable to see h2 tag because of foreground colours
 export default function Loading() {
-    // return <Lottie animationData={loadingAnimation} loop autoPlay />;
+    return <Lottie animationData={loadingAnimation} loop autoPlay />;
     // return <object data={loader} type="image/svg" className="h-24" />;
-    return <Image src={loader} alt="Loading" />;
+    // return <Image src={loader} alt="Loading" />;
     // const defaultOptions = {
     //     animationData: loadingAnimation,
     //     loop: true,
