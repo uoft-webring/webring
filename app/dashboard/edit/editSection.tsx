ffc873aac9438902976dc8540314c8d5023bffdc
Aman Meherally <amanmeherally@gmail.com>
2025-06-21 17:44:39 -0400

    Made edit page responsive

--- app/dashboard/edit/editSection.tsx
+++ app/dashboard/edit/editSection.tsx
@@ -27,25 +27,43 @@ export default function EditSection() {
     }
 
     return (
-        <Tabs defaultValue="edit" className="w-full lg:hidden">
-            <TabsList className="grid w-full grid-cols-2 mb-2">
-                <TabsTrigger value="edit">Edit</TabsTrigger>
-                <TabsTrigger value="preview">Preview</TabsTrigger>
-            </TabsList>
-            <TabsContent value="edit">
-                <h2>Edit your profile</h2>
-                <p className="mb-6">
-                    Complete the information below to complete your profile.
-                </p>
-                <EditForm formData={data} setFormData={setData} />
-            </TabsContent>
-            <TabsContent value="preview">
-                <h2>Preview</h2>
-                <p className="mb-6">
-                    Preview your profile live, as you make changes.
-                </p>
-                <ProfileCard userData={data} />
-            </TabsContent>
-        </Tabs>
+        <>
+            <Tabs defaultValue="edit" className="w-full lg:hidden">
+                <TabsList className="grid w-full grid-cols-2 mb-2">
+                    <TabsTrigger value="edit">Edit</TabsTrigger>
+                    <TabsTrigger value="preview">Preview</TabsTrigger>
+                </TabsList>
+                <TabsContent value="edit">
+                    <h2>Edit your profile</h2>
+                    <p className="mb-6">
+                        Complete the information below to complete your profile.
+                    </p>
+                    <EditForm formData={data} setFormData={setData} />
+                </TabsContent>
+                <TabsContent value="preview">
+                    <h2>Preview</h2>
+                    <p className="mb-6">
+                        Preview your profile live, as you make changes.
+                    </p>
+                    <ProfileCard userData={data} />
+                </TabsContent>
+            </Tabs>
+            <section className="hidden lg:flex items-start justify-center gap-12 [&>*]:grow [&>*]:shrink [&>*]:basis-0">
+                <div>
+                    <h2>Edit your profile</h2>
+                    <p className="mb-6 lg:text-base">
+                        Complete the information below to complete your profile.
+                    </p>
+                    <EditForm formData={data} setFormData={setData} />
+                </div>
+                <div>
+                    <h2>Preview</h2>
+                    <p className="mb-6 lg:text-base">
+                        Preview your profile live, as you make changes.
+                    </p>
+                    <ProfileCard userData={data} />
+                </div>
+            </section>
+        </>
     );
 }
