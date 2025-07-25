1a7ce3e8bcd40b09fe22226ad5c7f6f4a048b1da
mh-anwar <mohammad.anwar2939@gmail.com>
2025-07-25 05:24:25 -0400

    Improve logic, optimize and remove unused code

--- app/dashboard/DisplayToast.tsx
+++ app/dashboard/DisplayToast.tsx
@@ -2,27 +2,24 @@
 import { useEffect } from "react";
 import { ExternalToast, toast } from "sonner";
 
+type ToastTriggerProps = {
+    messageType: "warning" | "error" | "success";
+    message: string;
+};
+
 export default function ToastTrigger({
     messageType,
     message,
-}: {
-    messageType: "warning" | "error" | "success";
-    message: string;
-}) {
+}: ToastTriggerProps) {
     const options: ExternalToast = {
         duration: 999999,
         closeButton: true,
         position: "top-right",
     };
     useEffect(() => {
-        if (messageType === "error") {
-            toast.error(message, options);
-        } else if (messageType === "warning") {
-            toast.warning(message, options);
-        } else if (messageType === "success") {
-            toast.success(message, options);
-        }
-    }, []);
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors
+        toast[messageType](message, options);
+    }, [messageType, message, options]);
 
     return null;
 }
