1b1ab037363f644fb4528b4ab203d006c975d5a0
mh-anwar <mohammad.anwar2939@gmail.com>
2025-07-24 23:20:57 -0400

    Remove hidden errors, add source maps

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    devIndicators: false,
+    productionBrowserSourceMaps: true,
+
     eslint: {
         ignoreDuringBuilds: true,
     },
