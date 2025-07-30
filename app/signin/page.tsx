36154959eff184b77a65c15e5c71e4b1b3ad991e
mh-anwar <m.anwar@mail.utoronto.ca>
2025-07-30 05:06:02 -0400

    Use stricter typing, update imports, remove force-dynamic

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,12 +1,10 @@
-export const dynamic = "force-dynamic";
-
 import CardForm from "@/components/CardForm";
 import SigninForm from "./Form";
-import { getCurrentUser } from "../dashboard/actions";
+import { getAuthUser } from "../dashboard/actions";
 import { redirect } from "next/navigation";
 
 export default async function Signin() {
-    const { user } = await getCurrentUser();
+    const { user } = await getAuthUser();
 
     if (user) {
         redirect("/dashboard");
