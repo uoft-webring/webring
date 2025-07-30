629077b97640a714909aeb2a1dc4e4e6a4461740
mh-anwar <m.anwar@mail.utoronto.ca>
2025-07-30 05:08:58 -0400

    Refactor logic out to components, re-add Logo UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,42 @@
 import { Button } from "./ui/button";
 import { signOutAction } from "@/app/dashboard/actions";
-import { User } from "@supabase/supabase-js";
-import Logo from "./Logo";
-import Image from "next/image";
 import Link from "next/link";
+import FallbackImage from "./FallbackImage";
+import logo from "@/public/logo.svg";
+import { UserType } from "@/utils/zod";
+import Image from "next/image";
 
 type NavbarProps = {
-    user: User | null;
-    imageData: any;
+    user: UserType | null;
+    imageData: string;
 } & React.ComponentPropsWithoutRef<"nav">;
 
 export default function Navbar({ user, imageData }: NavbarProps) {
-    const userData = imageData;
-    const fallbackSrc = `https://api.dicebear.com/9.x/bottts-neutral/png?seed=${userData?.ring_id}&radius=50`;
-    const imageSrc = userData?.image_url || fallbackSrc;
-
     return (
         <nav className="max-w-[85rem] w-full px-6 py-4 flex justify-between items-center self-center z-999">
-            <Logo />
+            <Link href="/" className="w-48 lg:w-60">
+                <Image
+                    priority
+                    width={240}
+                    height={60}
+                    src={logo.src}
+                    alt="Logo"
+                    className="max-w-full max-h-full"
+                />
+            </Link>
             {user ? (
                 <div className="flex flex-row items-center justify-around gap-4">
                     <Button variant={"outline"} onClick={signOutAction}>
                         Sign out
                     </Button>
                     <Link href="/dashboard">
-                        <Image
-                            width={32}
-                            height={32}
-                            src={imageSrc}
-                            className="rounded-full w-14 aspect-square border-4 border-card outline-4 outline-white"
+                        <FallbackImage
+                            width={16}
+                            height={16}
+                            src={imageData}
+                            ringId={user.ring_id}
                             alt="Profile picture"
+                            className="rounded-2xl w-14 aspect-square border-1 border-card outline-2 outline-white"
                         />
                     </Link>
                 </div>
