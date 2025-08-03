efbace4b7564b8c1486cc52d1a90c00a862588b1
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-03 06:04:37 -0400

    Add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-background text-center">
+            <h2>Not Found</h2>
+            <p className="text-lg">
+                What's worse is, we couldn't even find a 404 page either.
+            </p>
+            <Link href="/" className="mt-4 text-blue-500 hover:underline">
+                Go back to home
+            </Link>
+        </div>
+    );
+}
