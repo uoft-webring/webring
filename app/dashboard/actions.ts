c9c20fb93bb2942c74d02bb274193ba385f625c9
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-03 06:05:15 -0400

    Improve logic, refactor for consistency, error handling

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -5,109 +5,180 @@ import { redirect } from "next/navigation";
 import { getDnsRecords } from "@layered/dns-records";
 import { createHmac } from "crypto";
 import { UserType } from "@/utils/zod";
+import { ApiResponse, getAuthUserProfile } from "../actions";
+import dns from "node:dns/promises";
+import { User } from "@supabase/supabase-js";
 
 // TODO: RLS to allow regular client to access this?
 // TODO: Should be an issue since this is on the server
 
-// TODO-A @TheAmanM please rewrite function names and descriptions to make more sense
-// Here, we fetch the verification status of a user from supabase.
-/*
-- fetch supabase and check if domain is verified
-- fetches txt records to check if record exists
-- fetches user data for user profile ?
-- check if they added code to portfolio
-*/
-export const getDomainStatus = async (): Promise<boolean> => {
+/**
+ * There is a clear distinction between domain vailidity and verification
+ * - Validity: Whether the user has added the code to their portfolio
+ * - Verification: Whether the user has verified their domain ownership through DNS records
+ */
+
+/**
+ * Only returns the domain verification *status* of the currently authenticated user.
+ *
+ * Checks if the user is verified by fetching the `is_verified` field in profile's table
+ *
+ * @returns {Promise<boolean>} A promise that resolves to `true` if the domain is verified, otherwise `false`.
+ * @throws {Error} If there is an error fetching the user profile or verification status.
+ */
+export const getDomainVerification = async (): Promise<boolean> => {
     const supabase = await createClient();
 
-    const { data: userData, error: dataError } = await getUserProfile();
+    // First obtain the ID of the currently authenticated user
+    const { data: user, error: userError } = await getAuthUserProfile();
+    if (!user || userError) return false;
 
-    if (!userData) {
-        return false;
-    }
-
-    console.log(`Fetching verification status for user ID: ${userData.id}`);
+    console.log(`Fetching verification status for user ID: ${user.id}`);
 
     const { data, error } = await supabase
         .from("profile") // Select the 'profiles' table
         .select("is_verified") // We only need the 'is_verified' column
-        .eq("id", userData.id); // Find the row where 'id' matches
+        .eq("id", user.id) // Find the row where 'id' matches
+        .single();
 
-    console.log(data);
-    if (!data) {
+    if (error || !data) {
         console.error("Error fetching verification status:", error.message);
         return false;
     }
 
-    const userObject = data.at(0);
-    if (userObject === undefined) {
-        return false;
-    }
-    return userObject.is_verified as boolean;
+    return Boolean(data.is_verified);
 };
 
-export const getTXTRecordValue = async (userId: string): Promise<string> => {
+/**
+ * Returns the TXT record value for domain verification.
+ *
+ * Generates a deterministic value based on the user's ID and a secret key.
+ *
+ * @returns {Promise<string>} A promise that resolves to the TXT record value.
+ * @throws {Error} If the secret key is not set in the environment variables.
+ * @param {string} userId - The ID of the user to generate the TXT record value for.
+ */
+export const getTXTRecordValue = async (seed: any): Promise<string> => {
     // Generate a secret-dependent deterministic TXT value from the user ID using a secret key
     // Increases security by preventing spoofing through ensuring only we can generate valid values
+    if (!process.env.DOMAIN_VALIDATION_SECRET_KEY) {
+        throw new Error(
+            "DOMAIN_VALIDATION_SECRET_KEY is not set in environment variables."
+        );
+    }
     return createHmac(
         "sha256",
         process.env.DOMAIN_VALIDATION_SECRET_KEY as string
     )
-        .update(userId) // based on user ID
+        .update(String(seed)) // based on seed
         .digest("base64url"); // safe for URL's
 };
 
-// Here we check whether the user has verified their domain or not from supabase.
+/**
+ * Returns whether the user has verified their domain or not *based on their TXT records*.
+ *
+ * Fetches the TXT records for the user's domain and checks if the expected TXT record exists.
+ * If the expected TXT record is found, it updates the user's profile to mark them as verified.
+ *
+ * @returns {Promise<boolean>} A promise that resolves to `true` if the domain is verified, otherwise `false`.
+ * @throws {Error} If there is an error fetching the user profile or updating the verification status.
+ */
 export const checkDomainRecords = async (): Promise<boolean> => {
     const supabase = await createClient();
 
-    const { data: userData, error: dataError } = await getUserProfile();
-
-    if (dataError) {
-        console.error("Error fetching user data:", dataError);
+    const { data: user, error: userError } = await getAuthUserProfile();
+    if (!user || userError) {
+        console.error("Error fetching user data:", userError);
         return false;
     }
 
-    let result: boolean = false;
-    const domainURL: URL = new URL(userData.domain);
-    const txtRecords = await getDnsRecords(domainURL.hostname, "TXT");
-    const expectedTxtValue = await getTXTRecordValue(userData.id);
+    const baseDomain = new URL(user.domain).hostname;
+    const expectedKey = `uoft-webring-${user.ring_id}`;
+    const fullRecordDomain = `${expectedKey}.${baseDomain}`;
+    const expectedValue = await getTXTRecordValue(user.ring_id);
+    try {
+        console.log("Expected Domain: " + fullRecordDomain);
+        console.log("Expected Value: " + expectedValue);
 
-    console.log("Expected Username: " + "uoft-webring-" + userData.id);
-    console.log("Expected Value: " + expectedTxtValue);
-    for (const record of txtRecords) {
-        if (
-            record.name === "uoft-webring-" + userData.id &&
-            record.data.includes(expectedTxtValue)
-        ) {
-            console.log("[CheckDomainRecords]: Domain Verified");
-            result = true;
+        const records = await dns.resolveTxt(fullRecordDomain);
+        const flatRecords = records.map((entry) => entry.join(""));
+
+        const matched = flatRecords.includes(expectedValue);
+
+        if (!matched) {
+            console.warn("No matching TXT record found for key and value.");
+            return false;
         }
-        console.log(record);
-    }
 
-    if (result) {
-        const { error } = await supabase
+        const { error: updateError } = await supabase
             .from("profile")
             .update({ is_verified: true })
-            .eq("id", userData.id)
+            .eq("id", user.id)
             .select();
 
-        if (error) {
-            console.error("Error updating domain verification status:", error);
+        if (updateError) {
+            console.error("Error updating verification status:", updateError);
             return false;
         }
 
         revalidatePath("/dashboard");
+        return true;
+    } catch (err) {
+        console.error("Unexpected error during domain verification:", err);
+        return false;
     }
-    return result as boolean;
+};
+
+//---------------------- DOMAIN VALIDITY ----------------------
+
+/**
+ * Only returns the domain validation *status* of the currently authenticated user.
+ *
+ * Checks if the user has validated their domain by fetching the `valid` field in profile's table
+ *
+ * @returns {Promise<boolean>} A promise that resolves to `true` if the domain is valid, otherwise `false`.
+ * @throws {Error} If there is an error fetching the user profile or validation status.
+ * @param {UserType} [user] - Optional user data. If not provided, it will be fetched.
+
+ */ export const getDomainValidity = async (
+    user?: UserType
+): Promise<boolean> => {
+    const supabase = await createClient();
+
+    if (!user) {
+        const { data, error } = await getAuthUserProfile();
+        if (error || !data) {
+            console.error("Failed to fetch authenticated user profile:", error);
+            return false;
+        }
+        user = data;
+    }
+
+    console.log(`Fetching verification status for user ID: ${user.id}`);
+
+    const { data, error } = await supabase
+        .from("profile")
+        .select("valid")
+        .eq("id", user.id)
+        .single();
+
+    console.log(data);
+    if (error || !data) {
+        console.error(
+            "Error fetching domain validation status:",
+            error.message
+        );
+        return false;
+    }
+
+    return data?.valid === true;
 };
 
 // Here we check whether the user has a valid portfolio or not from supabase.
 export const checkAddedCodeToPortfolio = async (): Promise<boolean> => {
     const supabase = await createClient();
 
-    const { data: userData, error: dataError } = await getUserProfile();
+    const { data: userData, error: dataError } = await getAuthUserProfile();
 
     if (dataError) {
         console.error("Error fetching user data:", dataError);
@@ -116,12 +187,12 @@ export const checkAddedCodeToPortfolio = async (): Promise<boolean> => {
 
     // TODO: Replace this with actual domain verification logic.
     const result: boolean = true;
-
+    //TODO need to write code to make a request in the DB
     if (result) {
         const { error } = await supabase
             .from("profile")
             .update({ valid: true })
-            .eq("id", userData.id)
+            .eq("id", userData?.id)
             .select();
 
         if (error) {
@@ -134,57 +205,28 @@ export const checkAddedCodeToPortfolio = async (): Promise<boolean> => {
     return result as boolean;
 };
 
-// Here we fetch the domain to see whether the user added the links on their page or not.
-export const getValidPortfolio = async (
-    userData?: UserType
-): Promise<boolean> => {
+/**
+ * Retrieves the currently authenticated user using the Supabase client
+ * from the Authentication table.
+ *
+ * @returns {Promise<ApiResponse<User>>} A response object containing the
+ * authenticated user in `data` if successful, or an error message in `error`
+ * if the operation fails.
+ */
+export const getAuthUser = async (): Promise<ApiResponse<User>> => {
     const supabase = await createClient();
-
-    if (!userData) {
-        const { data: fetchedUserData, error: dataError } =
-            await getUserProfile();
-        if (!fetchedUserData) {
-            return false;
-        }
-        userData = fetchedUserData;
-    }
-
-    console.log(`Fetching verification status for user ID: ${userData.id}`);
-
-    const { data, error } = await supabase
-        .from("profile")
-        .select("valid")
-        .eq("id", userData.id);
-
-    console.log(data);
-    if (!data) {
-        console.error("Error fetching verification status:", error?.message);
-        return false;
-    }
-
-    const userObject = data.at(0);
-    return userObject?.valid === true;
-};
-
-export const getAuthUser = async () => {
-    const supabase = await createClient(); // Get the Supabase client instance
-
     const { data, error } = await supabase.auth.getUser();
-    const user = data?.user || null; // Safely access user, default to null if data is null/undefined
 
-    return { user, error };
-};
-
-export const getUserProfile = async () => {
-    const supabase = await createClient();
-
-    const { data: userData, error: dataError } = await supabase
-        .from("profile")
-        .select("*");
+    if (error || !data?.user) {
+        return {
+            data: null,
+            error: error?.message ?? "User not found.",
+        };
+    }
 
     return {
-        data: userData?.at(0) as UserType,
-        error: dataError,
+        data: data.user,
+        error: null,
     };
 };
 
