3ab42a22976fc4986805f849549e8a63195038dd
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-03 05:59:24 -0400

    Add metadata, remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,67 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Oxanium } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 const oxanium = Oxanium({
     variable: "--font-oxanium",
     subsets: ["latin"],
 });
 
+const description =
+    "Join the UofT Webring to connect with other University of Toronto students and showcase your personal website.";
+
 export const metadata: Metadata = {
     metadataBase: new URL("https://uoftwebring.com"),
     title: "UofT Webring",
-    description:
-        "Join the UofT Webring to connect with other University of Toronto students and showcase your personal website.",
-
+    description,
+    keywords: [
+        "UofT Webring",
+        "University of Toronto",
+        "Webring",
+        "Student Community",
+    ],
+    /* OG and Twitter Images are auto-added by Next.js */
     openGraph: {
         title: "UofT Webring",
-        description:
-            "Join the UofT Webring to connect with other University of Toronto students and showcase your personal website.",
+        type: "website",
+        siteName: "UofT Webring",
+        description,
         url: "https://uoftwebring.com",
     },
     twitter: {
         card: "summary_large_image",
         title: "UofT Webring",
-        description:
-            "Join the UofT Webring to connect with other University of Toronto students and showcase your personal website.",
+        description,
     },
+    // We are fine with robots indexing our site
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+    },
+    referrer: "origin-when-cross-origin",
+    /* alternates: {
+        canonical: "/",
+    }, */
+    authors: [
+        {
+            name: "Jonathan Zhu",
+            url: "https://jonathanzhu.com",
+        },
+        {
+            name: "Mohammad Anwar",
+            url: "https://mohammadanwar.dev",
+        },
+        {
+            name: "Aman Meherally",
+            url: "https://amanmeherally.com/",
+        },
+        {
+            name: "Mohamad Damaj",
+            url: "https://mohamaddamaj.com",
+        },
+    ],
 };
 
 export default function RootLayout({
@@ -45,8 +70,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="scroll-smooth">
-            <body className={`${oxanium.variable} dark antialiased`}>
+        <html lang="en" className={oxanium.variable}>
+            <body className="scroll-smooth dark antialiased">
                 {children}
                 <Toaster richColors />
             </body>
