3df2423e9af54e960fc7dd638ab775de07b6b527
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-04 03:17:25 -0400

    Add basic sitemap

--- app/(metadata)/sitemap.ts
+++ app/(metadata)/sitemap.ts
@@ -1,1 +1,13 @@
 /*  We should make dynamic routes */
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://uoftwebring.com",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+    ];
+}
