9f3b0d15a9e60229fb3fb029b0b878ff18609407
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-09 23:04:40 -0400

    Complete slug logic (OG img and display)

--- app/u/[slug]/page.tsx
+++ app/u/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import { getUserProfile } from "@/app/actions";
 import Navbar from "@/components/Navbar";
 import ProfileCard from "@/components/ProfileCard";
+import { notFound } from "next/navigation";
 
 /**
  * This page is experimental
@@ -10,40 +11,19 @@ import ProfileCard from "@/components/ProfileCard";
 export default async function User({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
 
+    // We get the user profile from the database with the particular slug as their subdomain
     const { data, error } = await getUserProfile(slug);
-    const mockUserProfile = {
-        ring_id: 123,
-        name: "Mock User",
-        tagline: "This is a mock user profile",
-        domain: "mohammadanwar.dev",
-        valid: true,
-        github_url: "mh-anwar",
-        image_url: "https://avatars.githubusercontent.com/u/12345678?v=4",
-        is_verified: true,
-        validated_user_component: "disconnected",
-        tags: ["web", "developer", "mock"],
-        subdomain: slug,
-        graduation_year: 2024,
-        program: "Computer Science",
-    };
+
+    if (error || !data) {
+        notFound();
+    }
     return (
         <div className="min-h-screen bg-background flex flex-col">
             <Navbar />
 
             <div className="flex flex-col md:flex-row max-w-[70rem] mx-auto w-full flex-1  place-items-start">
                 <div className="max-w-[70rem] mx-auto w-full px-6 mt-12 sm:mt-6">
-                    {mockUserProfile ? (
-                        <ProfileCard user={mockUserProfile} />
-                    ) : (
-                        <>
-                            <h2 className="text-2xl font-semibold text-center mt-6">
-                                {error || "User not found"}
-                            </h2>
-                            <p className="text-center mt-4">
-                                The user profile you are looking for does not exist or has been removed.
-                            </p>
-                        </>
-                    )}
+                    <ProfileCard user={data} />
                 </div>
             </div>
         </div>
