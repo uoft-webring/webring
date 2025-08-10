76cc74ee630e0afc9e4ea74dfd7b79c574d91685
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-09 21:14:11 -0400

    Add spacing, fix width

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -36,15 +36,15 @@ export default async function DashboardLayout({ children }: { children: React.Re
         <UserProvider user={userData}>
             <div className="min-h-screen bg-background flex flex-col">
                 <Navbar user={userData} />
-                <section className="max-w-[70rem] mx-auto w-full px-6">
+                <section className="max-w-[85rem] mx-auto w-full px-6">
                     <h1 className="mb-4">{`Welcome, ${authUser.user_metadata.name}.`}</h1>
                 </section>
-                <div className="flex flex-col md:flex-row max-w-[70rem] mx-auto w-full flex-1  place-items-start">
+                <div className="flex flex-col md:flex-row max-w-[85rem] mx-auto w-full flex-1  place-items-start">
                     <Stepper
                         steps={onboardingRoutes}
                         className="px-6 w-full md:w-min md:h-screen md:flex md:flex-col  mt-12 sm:mt-6"
                     />
-                    <div className="max-w-[70rem] mx-auto w-full px-6 mt-12 sm:mt-6">{children}</div>
+                    <div className="max-w-[85rem] mx-auto w-full px-6 mt-12 sm:mt-6">{children}</div>
                 </div>
             </div>
         </UserProvider>
