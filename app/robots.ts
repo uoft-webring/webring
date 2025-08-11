6732198faa0b900cfdae06f9f3f4adf762b43be4
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-11 04:35:43 -0400

    Disallow certain paths for SEO crawling

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
+            disallow: ["/api", "/auth", "/dashboard", "/signin", "/signup", "/redirect"],
         },
         sitemap: "https://uoftwebring.com/sitemap.xml",
     };
