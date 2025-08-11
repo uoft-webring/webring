e1071c4d54d61f7a7873ab8e0b4d097b52aef920
mh-anwar <m.anwar@mail.utoronto.ca>
2025-08-11 04:30:21 -0400

    Redesign, remove JSX rendering

--- components/CodeSnippet.tsx
+++ components/CodeSnippet.tsx
@@ -1,61 +1,44 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import copyIcon from "@/icons/copy.svg";
+import checkIcon from "@/icons/clipboard-check.svg";
 import Prism from "prismjs";
 import prettier from "prettier/standalone";
-import estree from "prettier/plugins/estree";
-import tsPlugin from "prettier/plugins/typescript";
 import htmlPlugin from "prettier/plugins/html";
 import { Button } from "./ui/button";
-
+import { cn } from "@/lib/utils";
 import "prismjs/themes/prism-tomorrow.css"; // or prism-okaidia.css, prism.css, etc.
-
-import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-markup";
 
-type Lang = "jsx" | "html";
-
-export default function CodeSnippet({
-    codeString,
-    width = "100%",
-    lang = "jsx",
-}: {
-    codeString: string;
-    width?: string;
-    lang?: Lang;
-}) {
+export default function CodeSnippet({ codeString, width = "100%" }: { codeString: string; width?: string }) {
     const [copied, setCopied] = useState(false);
     const [html, setHtml] = useState("");
-
-    const grammar = useMemo(() => (lang === "html" ? Prism.languages.markup : Prism.languages.jsx), [lang]);
 
     useEffect(() => {
         let ignore = false;
         (async () => {
             let formatted = codeString;
 
-            // TS parser handles JSX
             formatted = await prettier.format(codeString, {
-                parser: lang === "html" ? "html" : "typescript",
-                plugins: [estree as any, tsPlugin as any, htmlPlugin as any],
+                parser: "html",
+                plugins: [htmlPlugin as any],
                 tabWidth: 4,
                 useTabs: false,
                 semi: false,
                 singleQuote: false,
-                trailingComma: "es5",
             });
 
             if (ignore) return;
-            const highlighted = Prism.highlight(formatted, grammar as any, lang);
+            const highlighted = Prism.highlight(formatted, Prism.languages.markup, "html");
             setHtml(highlighted);
         })();
 
         return () => {
             ignore = true;
         };
-    }, [codeString, grammar, lang]);
+    }, [codeString]);
 
     const handleCopy = async () => {
         await navigator.clipboard.writeText(codeString);
@@ -66,20 +49,26 @@ export default function CodeSnippet({
     return (
         <figure
             style={{ width }}
-            className="relative rounded-xl border bg-card text-card-foreground shadow-sm"
+            className={cn("flex rounded-xl border bg-card text-card-foreground shadow-sm")}
         >
-            <Button
-                size="icon"
-                variant="outline"
-                onClick={handleCopy}
-                className="absolute right-2 top-2 rounded-md px-2 py-1 text-xs"
-            >
-                {copied ? "✓" : <Image src={copyIcon} alt="Copy" className="size-4"></Image>}
-            </Button>
-
-            <pre className="rounded-xl p-4 text-md">
-                <code className={`language-${lang}`} dangerouslySetInnerHTML={{ __html: html }} />
+            {/* We expect a hydration warning, because SSR'd client component */}
+            <pre suppressHydrationWarning className="flex-1 rounded-l-xl p-4 text-md overflow-x-scroll">
+                <code className="language-html" dangerouslySetInnerHTML={{ __html: html }} />
             </pre>
+
+            <div className="p-2">
+                <Button
+                    size="icon"
+                    onClick={handleCopy}
+                    className="rounded-xl text-md !bg-slate-900 !opacity-100"
+                >
+                    {copied ? (
+                        <Image src={checkIcon} alt="Check" className="size-6 invert" width={25} height={25} />
+                    ) : (
+                        <Image src={copyIcon} alt="Copy" className="size-6" width={25} height={25} />
+                    )}
+                </Button>
+            </div>
         </figure>
     );
 }
