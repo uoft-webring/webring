df4f331b82bb9eb682fec5acae81eb92afd05910
jonathanzhu <jiasunzhu@gmail.com>
2025-08-10 22:11:42 -0400

    feat: image cropper on image upload

--- components/ImageCropper.tsx
+++ components/ImageCropper.tsx
@@ -1,126 +1,52 @@
 "use client";
 
-import { useRef, useState } from "react";
-import ReactCrop, { centerCrop, convertToPixelCrop, makeAspectCrop } from "react-image-crop";
-// import setCanvasPreview from "../setCanvasPreview";
+import ReactCrop, { Pick, Crop, centerCrop, makeAspectCrop, PixelCrop } from "react-image-crop";
+import { useState } from "react";
+import "react-image-crop/dist/ReactCrop.css";
 
-const ASPECT_RATIO = 1;
-const MIN_DIMENSION = 150;
+function createAspectCrop(mediaWidth: number, mediaHeight: number, minimumValue: number): Pick {
+    if (mediaWidth <= mediaHeight) {
+        return { unit: "%", width: minimumValue };
+    } else {
+        return { unit: "%", height: minimumValue };
+    }
+}
+// This is to demonstate how to make and center a % aspect crop
+// which is a bit trickier so we use some helper functions.
+function centerAspectCrop(
+    mediaWidth: number,
+    mediaHeight: number,
+    aspect: number,
+    minimumValue: number = 70
+) {
+    const aspectCrop: Pick = createAspectCrop(mediaWidth, mediaHeight, minimumValue);
 
-const ImageCropper = () => {
-    const imgRef = useRef(null);
-    const previewCanvasRef = useRef(null);
-    const [imgSrc, setImgSrc] = useState("");
-    const [crop, setCrop] = useState();
-    const [error, setError] = useState("");
+    return centerCrop(makeAspectCrop(aspectCrop, aspect, mediaWidth, mediaHeight), mediaWidth, mediaHeight);
+}
 
-    const onSelectFile = (e) => {
-        const file = e.target.files?.[0];
-        if (!file) return;
+export default function ImageCropper({ crop, setCrop, imageSrc, maxHeight }) {
+    const [completedCrop, setCompletedCrop] = useState<PixelCrop>();
+    const ASPECT_RATIO = 1;
 
-        const reader = new FileReader();
-        reader.addEventListener("load", () => {
-            const imageElement = new Image();
-            const imageUrl = reader.result?.toString() || "";
-            imageElement.src = imageUrl;
-
-            imageElement.addEventListener("load", (e) => {
-                if (error) setError("");
-                const { naturalWidth, naturalHeight } = e.currentTarget;
-                if (naturalWidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
-                    setError("Image must be at least 150 x 150 pixels.");
-                    return setImgSrc("");
-                }
-            });
-            setImgSrc(imageUrl);
-        });
-        reader.readAsDataURL(file);
-    };
-
-    const onImageLoad = (e) => {
+    function onImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
         const { width, height } = e.currentTarget;
-        const cropWidthInPercent = (MIN_DIMENSION / width) * 100;
-
-        const crop = makeAspectCrop(
-            {
-                unit: "%",
-                width: cropWidthInPercent,
-            },
-            ASPECT_RATIO,
-            width,
-            height
-        );
-        const centeredCrop = centerCrop(crop, width, height);
-        setCrop(centeredCrop);
-    };
+        console.log(width, height);
+        setCrop(centerAspectCrop(width, height, ASPECT_RATIO));
+    }
 
     return (
-        <>
-            <label className="block mb-3 w-fit">
-                <span className="sr-only">Choose profile photo</span>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={onSelectFile}
-                    className="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-2 file:rounded-full file:border-0 file:text-xs file:bg-gray-700 file:text-sky-300 hover:file:bg-gray-600"
-                />
-            </label>
-            {error && <p className="text-red-400 text-xs">{error}</p>}
-            {imgSrc && (
-                <div className="flex flex-col items-center">
-                    <ReactCrop
-                        crop={crop}
-                        onChange={(pixelCrop, percentCrop) => setCrop(percentCrop)}
-                        circularCrop
-                        keepSelection
-                        aspect={ASPECT_RATIO}
-                        minWidth={MIN_DIMENSION}
-                    >
-                        <img
-                            ref={imgRef}
-                            src={imgSrc}
-                            alt="Upload"
-                            style={{ maxHeight: "70vh" }}
-                            onLoad={onImageLoad}
-                        />
-                    </ReactCrop>
-                    {/*
-          <button
-            className="text-white font-mono text-xs py-2 px-4 rounded-2xl mt-4 bg-sky-500 hover:bg-sky-600"
-            onClick={() => {
-              setCanvasPreview(
-                imgRef.current, // HTMLImageElement
-                previewCanvasRef.current, // HTMLCanvasElement
-                convertToPixelCrop(
-                  crop,
-                  imgRef.current.width,
-                  imgRef.current.height
-                )
-              );
-              const dataUrl = previewCanvasRef.current.toDataURL();
-              updateAvatar(dataUrl);
-              closeModal();
+        <ReactCrop
+            crop={crop}
+            onChange={(crop, percentageCrop) => {
+                setCrop(percentageCrop);
             }}
-          >
-            Crop Image
-            </button>
-          */}
-                </div>
-            )}
-            {crop && (
-                <canvas
-                    ref={previewCanvasRef}
-                    className="mt-4"
-                    style={{
-                        display: "none",
-                        border: "1px solid black",
-                        objectFit: "contain",
-                        width: 150,
-                        height: 150,
-                    }}
-                />
-            )}
-        </>
+            onComplete={(c) => setCompletedCrop(c)}
+            aspect={ASPECT_RATIO}
+            // maxHeight={maxHeight}
+            minHeight={100}
+            circularCrop
+        >
+            <img src={imageSrc} alt="Crop avatar" onLoad={onImageLoad} />
+        </ReactCrop>
     );
-};
-export default ImageCropper;
+}
