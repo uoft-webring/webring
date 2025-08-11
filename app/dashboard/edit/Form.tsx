c22e23b94e49140d00e17df5a3aaf40836d8e6f5
jonathanzhu <jiasunzhu@gmail.com>
2025-08-10 22:11:42 -0400

    feat: image cropper on image upload

--- app/dashboard/edit/Form.tsx
+++ app/dashboard/edit/Form.tsx
@@ -24,8 +24,8 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Form from "next/form";
 import ImageCropper from "@/components/ImageCropper";
-import { Image } from "@react-three/drei";
 import { Crop } from "react-image-crop";
+import { max } from "three/src/nodes/TSL.js";
 
 type UserKeys = z.infer<ReturnType<typeof User.keyof>>;
 const NO_ERRORS = Object.fromEntries(User.keyof().options.map((key) => [key, undefined])) as Record<
@@ -52,6 +52,7 @@ export default function EditForm({
     const [errors, setErrors] = useState<Record<UserKeys, string | undefined>>(structuredClone(NO_ERRORS));
     const [open, setOpen] = useState(false);
     const [imageSrc, setImageSrc] = useState("");
+    const [crop, setCrop] = useState<Crop>();
 
     console.log("Rendering Parent with tags:", formData.tags);
 
@@ -95,6 +96,62 @@ export default function EditForm({
         console.log("subbed form");
         setOpen(false);
     };
+
+    const loadImage = (path: string) => {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            // img.crossOrigin = "Anonymous"; // to avoid CORS if used with Canvas
+            img.src = path;
+            img.onload = () => {
+                resolve(img);
+            };
+            img.onerror = (e) => {
+                reject(e);
+            };
+        });
+    };
+
+    /*
+     * Checks if an uploaded image
+     * @param {file} file that is uploaded by user
+     * @param {minWidth} minimum width of image defined
+     * @param {minHeight} minimum height of image defined
+     * @param {maxWidth} maximum width of image defined
+     * @param {maxHeight} minimum height of image defined
+     * @returns success status for image dimension check and "reason" for checkImageDimension
+     *          to fail, or empty string if check is successful
+     */
+    function checkImageDimensions(file, minWidth, minHeight, maxWidth, maxHeight) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                const img = new Image();
+                img.onload = () => {
+                    const isMaxWithinLimits = img.width < maxWidth && img.height < maxHeight;
+                    const isMinWithinLimits = img.width >= minWidth && img.height >= minHeight;
+                    if (!isMinWithinLimits) {
+                        resolve({ reason: "min", success: false, image: e.target.result });
+                    }
+                    if (!isMaxWithinLimits) {
+                        resolve({ reason: "max", success: false, image: e.target.result });
+                    }
+                    // console.log("within limits", isWithinLimits);
+                    resolve({ reason: "", success: true }); // resolve returns value for Promise
+                };
+                img.onerror = () => {
+                    reject(new Error("Failed to load image."));
+                };
+                img.src = e.target.result;
+            };
+
+            reader.onerror = () => {
+                reject(new Error("Failed to read file."));
+            };
+
+            reader.readAsDataURL(file);
+        });
+    }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {};
     // TODO-A check if Next Form can use server action for saving or smth
@@ -197,30 +254,73 @@ export default function EditForm({
                     }}
                     error={errors.github_url}
                 />
-                <Label htmlFor="image_url">Profile picture link</Label>
+                <Label htmlFor="image_url">Profile picture</Label>
                 <Dialog open={open}>
                     <DialogTrigger asChild>
                         <Input
                             name="image_url"
                             type="file"
-                            placeholder="https://yourdomain.com/profile.jpg"
+                            accept="image/*"
+                            // placeholder="https://yourdomain.com/profile.jpg"
                             required
                             defaultValue={formData.image_url ?? ""}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            onChange={async (e: React.ChangeEvent<HTMLInputElement>) => {
+                                setErrors({
+                                    ...errors,
+                                    image_url: "",
+                                });
+
                                 // Check if an image is uploaded
                                 if (e.target.files && e.target.files.length > 0) {
-                                    // setCrop(undefined);
+                                    // Enforce hard cap on image size
+                                    const maxAllowedMBSize = 5;
+                                    const maxAllowedSize = maxAllowedMBSize * 1024 * 1024;
+                                    // Get file size and compare
+                                    if (e.target.files[0].size > maxAllowedSize) {
+                                        e.target.value = "";
+                                        setErrors({
+                                            ...errors,
+                                            image_url: `Please upload an image smaller than ${maxAllowedMBSize}MB`,
+                                        });
+                                    }
+
+                                    // Enforce image dimensions
+                                    const minAllowedImageDimensions = 100;
+                                    const maxAllowedImageDimensions = 2048;
+
+                                    // Check if image uploaded passes image dimension check
+                                    const { reason, success } = await checkImageDimensions(
+                                        e.target.files[0],
+                                        minAllowedImageDimensions,
+                                        minAllowedImageDimensions,
+                                        maxAllowedImageDimensions,
+                                        maxAllowedImageDimensions
+                                    );
+
+                                    if (!success) {
+                                        e.target.value = "";
+                                        setErrors({
+                                            ...errors,
+                                            image_url:
+                                                reason === "min"
+                                                    ? `Min image width and height ${minAllowedImageDimensions}`
+                                                    : `Max image width and height ${maxAllowedImageDimensions}`,
+                                        });
+                                    }
+
+                                    setCrop(undefined);
+                                    setImageSrc("");
 
                                     const reader = new FileReader();
 
-                                    reader.addEventListener("load", () => {
+                                    reader.addEventListener("load", async () => {
                                         const file = reader?.result?.toString() || "";
                                         setImageSrc(file);
                                     });
 
                                     reader.readAsDataURL(e.target.files[0]);
 
-                                    console.log("user uploaded image");
+                                    console.log("user uploaded image", e.target.value);
                                     setOpen(true);
                                 }
                                 // saveToForm({
@@ -232,9 +332,9 @@ export default function EditForm({
                         />
                     </DialogTrigger>
                     <DialogContent showCloseButton={false} className="">
-                        <DialogTitle className="">Crop avatar</DialogTitle>
-                        <DialogDescription>Crop your uploaded profile avatar here.</DialogDescription>
-                        <ImageCropper imageSrc={imageSrc} />
+                        {/* <DialogTitle className="">Crop avatar</DialogTitle> */}
+                        {/* <DialogDescription></DialogDescription> */}
+                        <ImageCropper crop={crop} setCrop={setCrop} imageSrc={imageSrc} maxHeight={100} />
                         <Button onClick={() => saveImage()}>Save</Button>
                     </DialogContent>
                 </Dialog>
