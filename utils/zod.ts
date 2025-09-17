583159388184a9ec3c3dac45c528852fc5063f5f
jonathanzhu <jiasunzhu@gmail.com>
2025-09-16 21:47:37 -0400

    feat: Fix db query field names and change ImageCropper dimension crieria

--- utils/zod.ts
+++ utils/zod.ts
@@ -53,7 +53,7 @@ export const User = z.object({
             { message: "Invalid host format in URL." }
         )
         .refine(validateUrl, { message: "Please enter a live URL." }), // TODO: change to only fetch on save data
-    image_url: z.string().url({ message: "Please enter a valid URL." }).or(z.literal("")),
+    image_key: z.string().url({ message: "Please enter a valid URL." }).or(z.literal("")),
     is_verified: z.boolean(),
     validated_user_component: z.string(),
     github_url: z.string().nullable(),
@@ -72,15 +72,15 @@ export const User = z.object({
         .max(60, { message: "Program name must be at most 60 characters." })
         .regex(/^[a-zA-Z0-9\s&(),.-]+$/, { message: "Program name contains invalid characters." })
         .nullable(),
-    subdomain: z
+    slug: z
         .string()
-        .min(2, { message: "Subdomain must be at least 2 characters." })
-        .max(30, { message: "Subdomain must be at most 30 characters." })
+        .min(2, { message: "Slug must be at least 2 characters." })
+        .max(30, { message: "Slug must be at most 30 characters." })
         .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
-            message: "Subdomain can only contain lowercase letters, numbers, and hyphens.",
+            message: "Slug can only contain lowercase letters, numbers, and hyphens.",
         })
         .refine((val) => !val.startsWith("-") && !val.endsWith("-"), {
-            message: "Subdomain cannot start or end with a hyphen.",
+            message: "Slug cannot start or end with a hyphen.",
         })
         .nullable(),
 });
