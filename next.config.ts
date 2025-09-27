e24ab25feeb7983296ae4eefbb5155e8b8e1be9b
mh-anwar <m.anwar@mail.utoronto.ca>
2025-09-27 02:24:14 -0400

    Remove mdx

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
-
-const withMDX = createMDX({
-    extension: /\.mdx?$/,
-});
 
 const cloudfrontDomain = process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN;
 if (!cloudfrontDomain) {
     throw new Error("Missing env var: NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN");
 }
 const nextConfig: NextConfig = {
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    pageExtensions: ["js", "jsx", "ts", "tsx"],
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
@@ -55,4 +50,4 @@ const nextConfig: NextConfig = {
     },
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
