bc3b7f1ac941049da7101e5995811e1d589efe7d
mh-anwar <m.anwar@mail.utoronto.ca>
2025-11-02 19:44:26 -0500

    Fix email checking

--- supabase/schemas/auth.sql
+++ supabase/schemas/auth.sql
@@ -1,7 +1,7 @@
 -- create function get_confirmation_sent(user_email text) returns text as $$
---     declare 
+--     declare
 --         confirmation text;
---     begin 
+--     begin
 --         select confirmation_sent_at into confirmation
 --         from auth.users
 --         where email = user_email;
@@ -15,11 +15,11 @@ revoke all on public.users from anon, authenticated;
 grant select on table public.users to service_role;
 
 -- Create user profile when a new user is created in auth.users
-create function handle_new_user() 
+create function handle_new_user()
 returns trigger as $$
 
 begin
-    insert into public.profile(id, email, name, domain, subdomain) 
+    insert into public.profile(id, email, name, domain, subdomain)
     values (new.id, new.email, new.raw_user_meta_data->>'name', new.raw_user_meta_data->>'domain', gen_random_uuid()::text); -- remember to have the single quotes this!
 
     return new;
@@ -27,8 +27,8 @@ end;
 $$ language plpgsql security definer set search_path = auth, public;
 
 -- Create trigger that will activate handle_new_user whenever new row is added into auth.users
-create trigger on_auth_new_user 
-    after insert on auth.users 
+create trigger on_auth_new_user
+    after insert on auth.users
     for each row execute function handle_new_user();
 
 -- Create function to check if user inputted email is a uoft email
@@ -36,7 +36,7 @@ create function check_uoft_email()
 returns trigger as $$
 
 begin
-    if position(reverse('utoronto.ca') in reverse(new.email)) = 0 then
+    if position(reverse('utoronto.ca') in reverse(new.email)) = 1 then
         return new;
     else
         return null;
