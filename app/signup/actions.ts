0af626bea433546fc9ab4e112f108d8ef74e84fc
mh-anwar <m.anwar@mail.utoronto.ca>
2026-04-12 02:50:31 -0400

    fix: use VERCEL_ENV to detect preview deploys for magic link redirect

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -16,7 +16,7 @@ export const signUpAction = async (name: string, email: string) => {
         email,
         options: {
             shouldCreateUser: true,
-            emailRedirectTo: `${process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXT_PUBLIC_HOME_DOMAIN}/auth/callback`,
+            emailRedirectTo: `${process.env.VERCEL_ENV === "preview" ? `https://${process.env.VERCEL_URL}` : process.env.NEXT_PUBLIC_HOME_DOMAIN}/auth/callback`,
             data: {
                 //  attach user metadata
                 name: name,
